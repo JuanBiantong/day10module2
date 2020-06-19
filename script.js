a712b9a3034996b1cdb68b3c6d886df3975fbb40
JuanBiantong <jbiantong@gmail.com>
2020-06-19 15:26:20 +0700

    try again

---
+++ script.js
@@ -0,0 +1,5 @@
+import {addition,subtraction,multiplication,divison} from './script1.js'
+console.log(addition(3,5))
+console.log(subtraction(3,5))
+console.log(multiplication(3,5))
+console.log(divison(3,5))
